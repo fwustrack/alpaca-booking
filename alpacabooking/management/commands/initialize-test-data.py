9f636c7ebb3e4028532e359547bc8dc7183841dd
Andreas Hemel <daishan@users.noreply.github.com>
2025-05-06 15:40:07 +0200

    fix logging in initialize-test-data

--- alpacabooking/management/commands/initialize-test-data.py
+++ alpacabooking/management/commands/initialize-test-data.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 from datetime import date, datetime, time, timedelta
 
 from django.core.management.base import BaseCommand
@@ -6,12 +7,14 @@ from django.utils import timezone
 
 from alpacabooking.models import Event, EventType
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     help = "Creates test data for manual testing"
 
     def handle(self, *args, **options):
-        print("creating test data")
+        logger.info("creating test data")
         ww = EventType.objects.get_by_natural_key("Weinwanderung")
         asw = EventType.objects.get_by_natural_key("Alpakerol Spritz Wanderung")
         mf = EventType.objects.get_by_natural_key("Meet & Feed")
@@ -21,12 +24,13 @@ class Command(BaseCommand):
         self.create_event(asw, in_days=5, hour=11)
         self.create_event(asw, in_days=10, hour=11)
         self.create_event(mf, in_days=5, hour=11)
+        logger.info("creating test data complete")
 
     def create_event(self, event_type: EventType, in_days: int, hour: int) -> None:
         naive_start_time = datetime.combine(date.today() + timedelta(days=in_days), time(hour))
         start_time = timezone.make_aware(naive_start_time)
         if len(Event.objects.filter(event_type=event_type, start_time=start_time)) > 0:
-            print("Event already exists")
+            logger.info("Event already exists")
         else:
             ev = Event()
             ev.event_type = event_type

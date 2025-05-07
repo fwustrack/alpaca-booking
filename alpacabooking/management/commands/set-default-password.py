5d5253a96930579982ce2dbe9f581f0ba64f322a
Andreas Hemel <daishan@users.noreply.github.com>
2025-05-07 17:42:03 +0200

    add initialize-data.sh and move passwords to secrets env file

---
+++ alpacabooking/management/commands/set-default-password.py
@@ -0,0 +1,24 @@
+import logging
+import os
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Set the default password for a user"
+
+    def add_arguments(self , parser):
+        parser.add_argument('user')
+
+    def handle(self, *args, **options):
+        username = options['user']
+        logger.info('setting default password for ' + username)
+        user: User = User.objects.get_by_natural_key(username)
+        pw = os.environ.get("DEFAULT_USER_PASSWORD")
+        if not pw:
+            raise Exception("missing password")
+        user.set_password(pw)
+        user.save()
---
+++ initialise-data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./runmanage.sh migrate
+./runmanage.sh createsuperuser --noinput
+./runmanage.sh loaddata base-data groups
+./runmanage.sh initialize-test-data
+
+./runmanage.sh set-default-password laura
+./runmanage.sh set-default-password felix

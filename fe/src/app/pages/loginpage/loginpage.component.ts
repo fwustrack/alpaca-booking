c21a51d523e0208d945e8b28d7dff81dceb97982
Adrian Hitsch <adrian.hitsch@outlook.de>
2025-06-15 17:34:39 +0200

    Refactor components and styles

--- fe/src/app/pages/loginpage/loginpage.component.ts
+++ fe/src/app/pages/loginpage/loginpage.component.ts
@@ -1,21 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { catchError, EMPTY } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
-import { LoginComponent } from '../../components/login/login.component';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [LoginComponent],
+  imports: [PasswordModule, ReactiveFormsModule, InputTextModule, IftaLabelModule, ButtonModule],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.scss',
 })
 export class LoginpageComponent {
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-  ) {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  loginForm = this.fb.group({
+    username: [''],
+    password: [''],
+  });
+
+  constructor() {
     if (this.authService.isTokenValid()) {
       this.router.navigate(['/']);
     }
+  }
+
+  onSubmit() {
+    const { username, password } = this.loginForm.value;
+
+    if (!username || !password) {
+      alert('Bitte E-Mail und Passwort eingeben');
+      return;
+    }
+
+    this.authService
+      .login(username, password)
+      .pipe(
+        catchError(() => {
+          alert('Login fehlgeschlagen');
+          return EMPTY;
+        }),
+      )
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
   }
 }
